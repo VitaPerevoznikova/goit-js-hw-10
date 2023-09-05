e95c254022effa656ea31e8ee2c03afaad5c358f
VitaPerevoznikova <frayvita@gmail.com>
2023-09-05 15:35:51 +0300

    add api

---
+++ src/config/api.js
@@ -0,0 +1,2 @@
+export const BASE_URL = "https://api.thecatapi.com/v1/images/search";
+export const API_KEY = "x-api-key: live_LRkzks08PAavXHHRSeKjPCWDogGgFHlsyqlYngH5q0KZC6GXAmBdxSJpOvCMpnHK";
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { API_KEY, BASE_URL } from "./config/api";
+
+const select = document.querySelector(".breed-select");
+const catInfo = document.querySelector(".cat-info");
+
+
+
